4583c041db3b8573802fd18f068ea50a18fe6d70
dsjlee <damonsjl@gmail.com>
2016-12-03 22:56:39 -0800

    initial commit

---
+++ Proto/Scripts/app/main.ts
@@ -0,0 +1,10 @@
+﻿class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return "<h1>" + this.greeting + "</h1>";
+    }
+};
+
+var greeter = new Greeter("Hello, world!");
+
+document.getElementById('title').innerHTML = greeter.greet();
