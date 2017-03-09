9dc8f8a666cb71fb9dce217e46bd18139d69fa46
dsjlee <damonsjl@gmail.com>
2017-03-08 21:36:05 -0800

    testing es6 features

--- Proto/Scripts/app/appcontroller.js
+++ Proto/Scripts/app/appcontroller.js
@@ -1,15 +1,20 @@
 var AppSpace;
 (function (AppSpace) {
-    var AppController = (function () {
-        function AppController() {
-            var _this = this;
-            this.alertCtrlName = function () {
-                alert(_this.ctrlName);
+    class AppController {
+        constructor() {
+            this.numList = [1, 2, 3, 4, 5];
+            this.alertCtrlName = () => {
+                alert(this.ctrlName);
+            };
+            this.alertNumbers = () => {
+                console.log(this.numList.find((x) => x < 2));
+                this.numList.forEach((value, index) => {
+                    console.log(Number.isNaN(value));
+                });
             };
             this.ctrlName = 'AppController';
         }
-        return AppController;
-    }());
+    }
     AppController.$inject = [];
     AppSpace.AppController = AppController;
 })(AppSpace || (AppSpace = {}));
--- Proto/Scripts/app/appcontroller.ts
+++ Proto/Scripts/app/appcontroller.ts
@@ -5,6 +5,7 @@
         static $inject = [];
         
         ctrlName: string;
+        numList: number[] = [1, 2, 3, 4, 5];
 
         constructor() {
             this.ctrlName = 'AppController';
@@ -12,6 +13,14 @@
         
         alertCtrlName = () => {
             alert(this.ctrlName);
+        }
+
+        alertNumbers = () => {
+            console.log(this.numList.find((x) => x < 2));
+
+            this.numList.forEach((value, index) => {
+                console.log(Number.isNaN(value));                
+            });
         }
     }
 }
