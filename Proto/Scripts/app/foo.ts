c77c85af5bfe38e39ec54ae96cadd51e3849ad34
dsjlee <damonsjl@gmail.com>
2017-03-16 20:25:16 -0700

    remove unused file

--- Proto/Scripts/app/foo.ts
+++
@@ -1,9 +0,0 @@
-﻿console.log(Number.isNaN(NaN));
-
-var users = [
-    { 'user': 'barney', 'age': 36, 'active': true },
-    { 'user': 'fred', 'age': 40, 'active': false },
-    { 'user': 'pebbles', 'age': 1, 'active': true }
-]
-
-users.find(function (o) { return o.age < 40; });
