7ec71041d3328782a224184c20034a539d3f4653
dsjlee <dsjlee@live.com>
2017-08-05 12:38:05 -0700

    add signalr typing

--- Proto/app/app.js
+++ Proto/app/app.js
@@ -1,4 +1,5 @@
 "use strict";
+/// <reference path="../scripts/typings/signalr/signalr.d.ts" />
 /// <reference path="../scripts/typings/angularjs/angular.d.ts" />
 angular.module('app', [])
     .controller('appController', AppSpace.AppController);
--- Proto/app/app.ts
+++ Proto/app/app.ts
@@ -1,4 +1,5 @@
-﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+﻿/// <reference path="../scripts/typings/signalr/signalr.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
 
 angular.module('app', [])
     .controller('appController', AppSpace.AppController as ng.Injectable<ng.IControllerConstructor>);
