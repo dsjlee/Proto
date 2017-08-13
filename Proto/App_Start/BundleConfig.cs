f4c37bc7efccccaec9421da4c297c1f15fa996f5
dsjlee <dsjlee@live.com>
2017-08-13 05:35:02 -0700

    font awesome spinner for reconnect button

--- Proto/App_Start/BundleConfig.cs
+++ Proto/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Proto
                         "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/font-awesome.min.css",
                         "~/Content/bootstrap.css",
                         "~/Content/site.css"));
 
